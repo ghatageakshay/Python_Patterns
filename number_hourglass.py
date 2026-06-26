b39e3a11bb1e8c9beed2d6a89b71de4810d16947
ghatageakshay <ghatageakshay96@gmail.com>
2026-06-26 23:19:17 +0530

    numbered houglass

---
+++ number_hourglass.py
@@ -0,0 +1,20 @@
+#number HourGlass Pattern
+n = 5
+
+for i in range(n):
+    for k in range(i):
+        print(" ",end = " ")
+
+    for j in range(2 * (n - i) - 1):
+        print(j , end = " ")
+
+    print()
+
+for i in range(n):
+    for k in range(n -i - 1):
+        print(" ",end= " ")
+
+    for j in range(2 * i + 1):
+        print(j,end = " ")
+
+    print()
